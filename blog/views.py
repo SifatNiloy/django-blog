43cdee444356fa1cf0744c885a61a4c9b8a4ea72
SifatNiloy <sifatniloy18@gmail.com>
2023-07-18 16:22:24 +0600

    added urls and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
+from . import views
+
 # Create your views here.
+
+def starting_page(request):
+    pass
+
+def posts(request):
+    pass
+
+def post_detail(request):
+    pass
+
+
+
