b8903f22aa03930ff42cc073ab052c1e38660da8
SifatNiloy <sifatniloy18@gmail.com>
2023-08-09 04:06:17 +0600

    dynamic link created for blogs

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,76 @@
+from datetime import date
+
 from django.shortcuts import render
+
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "sifat",
+        "date": date(2023, 7, 29),
+        "title": "Mountain Hiking",
+        "excerpt": "There is nothing like the views you get when you hiking the mountains. And I wasn't even prepared what happened while I was enjoying the view.",
+        "content": """
+            Astronomy is the study of everything in the universe beyond Earth's atmosphere. That includes objects we can see with our naked eyes, like the Sun , the Moon , the planets, and the stars . It also includes objects we can only see with telescopes or other instruments, like faraway galaxies and tiny particles.
+
+            Astronomy is the study of everything in the universe beyond Earth's atmosphere. That includes objects we can see with our naked eyes, like the Sun , the Moon , the planets, and the stars . It also includes objects we can only see with telescopes or other instruments, like faraway galaxies and tiny particles.
+            
+            Astronomy is the study of everything in the universe beyond Earth's atmosphere. That includes objects we can see with our naked eyes, like the Sun , the Moon , the planets, and the stars . It also includes objects we can only see with telescopes or other instruments, like faraway galaxies and tiny particles.
+        """
+    },
+    {
+        "slug": "Programming-is-fun",
+        "image": "coding.jpg",
+        "author": "sifat",
+        "date": date(2023, 6, 20),
+        "title": "Programming is fun",
+        "excerpt": "There is nothing like the views you get when you hiking the mountains. And I wasn't even prepared what happened while I was enjoying the view.",
+        "content": """
+            Astronomy is the study of everything in the universe beyond Earth's atmosphere. That includes objects we can see with our naked eyes, like the Sun , the Moon , the planets, and the stars . It also includes objects we can only see with telescopes or other instruments, like faraway galaxies and tiny particles.
+
+            Astronomy is the study of everything in the universe beyond Earth's atmosphere. That includes objects we can see with our naked eyes, like the Sun , the Moon , the planets, and the stars . It also includes objects we can only see with telescopes or other instruments, like faraway galaxies and tiny particles.
+            
+            Astronomy is the study of everything in the universe beyond Earth's atmosphere. That includes objects we can see with our naked eyes, like the Sun , the Moon , the planets, and the stars . It also includes objects we can only see with telescopes or other instruments, like faraway galaxies and tiny particles.
+        """
+    },
+    {
+        "slug": "Into-the-forest",
+        "image": "woods.jpg",
+        "author": "sifat",
+        "date": date(2023, 5, 9),
+        "title": "Into the forest",
+        "excerpt": "There is nothing like the views you get when you hiking the mountains. And I wasn't even prepared what happened while I was enjoying the view.",
+        "content": """
+            Astronomy is the study of everything in the universe beyond Earth's atmosphere. That includes objects we can see with our naked eyes, like the Sun , the Moon , the planets, and the stars . It also includes objects we can only see with telescopes or other instruments, like faraway galaxies and tiny particles.
+
+            Astronomy is the study of everything in the universe beyond Earth's atmosphere. That includes objects we can see with our naked eyes, like the Sun , the Moon , the planets, and the stars . It also includes objects we can only see with telescopes or other instruments, like faraway galaxies and tiny particles.
+            
+            Astronomy is the study of everything in the universe beyond Earth's atmosphere. That includes objects we can see with our naked eyes, like the Sun , the Moon , the planets, and the stars . It also includes objects we can only see with telescopes or other instruments, like faraway galaxies and tiny particles.
+        """
+    },
+]
+
+
+def get_date(post):
+    return post["date"]
 
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    identified_post = next( post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html",{
+        "post": identified_post
+    })
